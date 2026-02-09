8493c584f401a5c2fa6ba3061b2b392c31033d9c
bellodev <fernandojbf123@gmail.com>
2026-02-09 17:15:06 -0600

    Programa terminado. Hay que estar pendiente con los datos de las boyas que tuvieron problemas ya que la cantidad de datos esperadas se calcula suponiendo que todo opera bien en la boya

---
+++ funciones.py
@@ -0,0 +1,141 @@
+################### NO TOCAR ESTE ARCHIVO ###################
+import os
+import pandas as pd
+import numpy as np
+
+
+def crear_ruta_a_archivo(nombre_archivo, ruta_a_carpeta="data"):
+    """Crea una ruta completa a un archivo dentro de una carpeta especificada."""
+    return os.path.join(ruta_a_carpeta, nombre_archivo)
+
+def cargar_csv(ruta_archivo):
+    """Carga un archivo CSV y devuelve un DataFrame de pandas."""
+    if not os.path.exists(ruta_archivo):
+        raise FileNotFoundError(f"El archivo {ruta_archivo} no existe.")
+    return pd.read_csv(ruta_archivo)
+
+def estandarizar_nombres_de_columnas(df):
+    """Estandariza los nombres de las columnas de un DataFrame."""
+    df.columns.values[0] = "fecha"
+    return df
+
+def guardar_csv(df, ruta_archivo):
+    """Guarda un DataFrame de pandas en un archivo CSV."""
+    if not os.path.exists(os.path.dirname(ruta_archivo)):
+        os.makedirs(os.path.dirname(ruta_archivo))
+    df.to_csv(ruta_archivo, index=False)
+    
+def buscar_archivos_csv(ruta_a_carpeta):
+    """Busca y devuelve una lista de archivos CSV en la carpeta especificada."""
+    if not os.path.exists(ruta_a_carpeta):
+        raise FileNotFoundError(f"La carpeta {ruta_a_carpeta} no existe.")
+    return [f for f in os.listdir(ruta_a_carpeta) if f.endswith('.csv')]
+
+def arreglar_fechas_de_df(df):
+    """Convierte la columna 'fecha' de un DataFrame a formato datetime."""
+    # 2026-01-01T00:02:00.000z
+    df['fecha'] = pd.to_datetime(df["fecha"])
+    return df
+
+def filtrar_fechas(df,fecha_inicial, fecha_final):
+    """Filtra un DataFrame por un rango de fechas."""
+    fecha_inicial = pd.to_datetime(fecha_inicial, format ="%Y-%m-%d %H:%M:%S").replace(second=0, microsecond=0)
+    fecha_final = pd.to_datetime(fecha_final, format ="%Y-%m-%d %H:%M:%S").replace(second=0, microsecond=0)
+        
+    df['fecha'] = pd.to_datetime(df['fecha'])
+    return df[(df['fecha'] >= fecha_inicial) & (df['fecha'] <= fecha_final)]
+
+def obtener_nombre_de_boya(ruta_a_archivo):
+    """Obtiene el nombre de la boya a partir del nombre del archivo."""
+    nombre = ruta_a_archivo.split('_')
+    return "".join(nombre[2:3])
+
+def buscar_archivos_csv_de_una_boya(ruta_a_carpeta, nombre_de_boya):
+    """Busca y devuelve una lista de archivos CSV de una boya específica en la carpeta."""
+    archivos = buscar_archivos_csv(ruta_a_carpeta)
+    archivos_boya = []
+    for archivo in archivos:
+        if nombre_de_boya in archivo:
+            archivos_boya.append(archivo)
+    return archivos_boya
+
+def crear_diccionario_de_boyas(archivos: list[str]) -> dict:
+    """Crea un diccionario de boyas a partir de una lista de archivos."""
+    dic = {}
+    for archivo in archivos:
+        nombre_de_boya = obtener_nombre_de_boya(archivo)
+        if nombre_de_boya not in dic:
+            dic[nombre_de_boya] = {
+                "viento": pd.DataFrame(),
+                "oleaje": pd.DataFrame(),
+                "corriente": pd.DataFrame(),
+                "mct": pd.DataFrame(),
+            }
+    return dic
+
+
+def llenar_datos_de_diccionario_de_boyas(dic: dict, ruta_a_carpeta: str, fecha_inicial: str, fecha_final: str):
+    for nombre_de_boya in dic:
+        archivos_de_boya = buscar_archivos_csv_de_una_boya(ruta_a_carpeta, nombre_de_boya)
+        for archivo in archivos_de_boya:
+            ruta_a_archivo = crear_ruta_a_archivo(archivo, ruta_a_carpeta)
+            df = cargar_csv(ruta_a_archivo)
+            df = estandarizar_nombres_de_columnas(df)
+            df = arreglar_fechas_de_df(df)
+            tipo = obtener_tipo_de_dato(archivo)
+            df_filtrado = filtrar_fechas(df, fecha_inicial, fecha_final) 
+            dic[nombre_de_boya][tipo] = df_filtrado
+    return dic
+
+def obtener_tipo_de_dato(archivo: str) -> str:
+    """Obtiene el tipo de dato (viento, oleaje, corriente, mct) a partir del nombre del archivo."""
+    if "Viento" in archivo:
+        return "viento"
+    elif "Oleaje" in archivo or "Ola" in archivo:
+        return "oleaje"
+    elif "Corriente" in archivo:
+        return "corriente"
+    elif "Salinidad" in archivo:
+        return "mct"
+    else:
+        raise ValueError(f"No se pudo determinar el tipo de dato para el archivo {archivo}.")
+    
+    
+def crear_dataframe_de_salida(dic: dict, fecha_inicial, fecha_final) -> pd.DataFrame:
+    """Crea un DataFrame de salida a partir del diccionario de boyas."""
+    tipos = ["viento","oleaje","corriente","mct"]
+    df_salida = pd.DataFrame(columns=["boya"])
+    for tipo in tipos:
+        esperados = []
+        recibidos = []
+        for boya in dic.keys():
+            if len(dic[boya][tipo]) == 0:
+                esperados.append(0)
+                recibidos.append(0)
+                continue
+            
+            delta_t_min = (dic[boya][tipo]["fecha"].diff().dt.total_seconds() /60).min()
+            cantidad_de_datos_esperados = len(pd.date_range(start=fecha_inicial, end=fecha_final, freq=f"{delta_t_min}min"))
+            if tipo == "viento":
+                var_name = dic[boya][tipo].columns[4]
+            else:
+                var_name = dic[boya][tipo].columns[1]
+            
+            dic[boya][tipo].dropna(subset=var_name, inplace=True)
+            cantidad_de_datos_recibidos = len(dic[boya][tipo])
+            esperados.append(cantidad_de_datos_esperados)
+            recibidos.append(cantidad_de_datos_recibidos)
+            
+        df_salida["boya"] = pd.Series(list(dic.keys()))
+        df_salida[f"{tipo}_esperados"] = pd.Series(esperados)
+        df_salida[f"{tipo}_recibidos"] = pd.Series(recibidos) 
+    
+    return df_salida
+
+def guardar_excel(df, ruta_a_carpeta_de_guardado, nombre_de_excel_de_salida):
+    """Guarda el DataFrame de salida en un archivo Excel."""    
+    ruta_a_archivo = crear_ruta_a_archivo(f"{nombre_de_excel_de_salida}.xlsx", ruta_a_carpeta_de_guardado)
+    if not os.path.exists(os.path.dirname(ruta_a_archivo)):
+        os.makedirs(os.path.dirname(ruta_a_archivo))
+    df.to_excel(ruta_a_archivo, index=False)
+    print(f"Archivo guardado en: {ruta_a_archivo}")
--- readme.md
+++ readme.md
@@ -1,1 +1,53 @@
-Aca va el readme
+# Determinar Porcentajes
+
+Este proyecto contiene un código en Python que carga datos desde un archivo en formato CSV. El programa calcula la cantidad de datos esperada y la cantidad de datos recibidos, permitiendo así verificar la integridad y completitud de los datos procesados.
+
+Además, este archivo busca y lee automáticamente todos los archivos `.csv` que se encuentren en la ruta de destino y los ordena por nombre de boya. Si solo se desean cargar datos de dos boyas, entonces en la carpeta solo deben estar los archivos `.csv` correspondientes a esas dos boyas. Las variables que no estén disponibles para esas boyas se colocarán en cero automáticamente.
+
+## Descripción
+
+- **Carga de datos:** El código lee archivos CSV para procesar la información contenida.
+- **Cálculo de datos esperados y recibidos:** Se determina cuántos datos deberían estar presentes y cuántos realmente se han recibido, facilitando la validación de los datos.
+
+## Instalación
+
+
+Se recomienda utilizar un ambiente virtual para evitar conflictos con otras dependencias de Python. Siga los siguientes pasos:
+
+1. **Cree el ambiente virtual:**
+
+   En Windows:
+   ```powershell
+   python -m venv venv
+   ```
+   En macOS/Linux:
+   ```bash
+   python3 -m venv venv
+   ```
+
+2. **Active el ambiente virtual:**
+
+   En Windows:
+   ```powershell
+   .\venv\Scripts\activate
+   ```
+   En macOS/Linux:
+   ```bash
+   source venv/bin/activate
+   ```
+
+3. **Instale las dependencias:**
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## Requisitos
+
+- Python 3.14.2
+
+Asegúrese de tener instalada la versión 3.14.2 de Python para garantizar la compatibilidad del código.
+
+## Uso
+
+Ejecute el código principal siguiendo las instrucciones del archivo `main.ipynb` o utilizando los scripts proporcionados.
